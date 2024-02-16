976c9c6f31ac3e336193e27693ddce539dcfea17
Rajesh Koladiya <rdkoladiya1471@gmail.com>
2024-02-16 19:09:51 +0530

    add my

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        As a seasoned MERN stack developer with over one year of hands-on
+        experience, I have demonstrated proficiency in leveraging MongoDB,
+        Express.js, React, and Node.js to design and implement full-stack
+        solutions. My expertise extends to developing responsive and
+        user-friendly interfaces, optimizing server-side performance, and
+        integrating secure and efficient data storage solutions. With a keen eye
+        for detail and a commitment to staying abreast of the latest industry
+        trends, I have successfully contributed to the creation of scalable and
+        modern web applications. My diverse skill set, coupled with a proven
+        track record of delivering high-quality projects, positions me as a
+        valuable asset for any team seeking a MERN stack developer with a strong
+        foundation in both frontend and backend technologies.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, "about");
